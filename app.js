0735548e6b60444a11f7d77356a0c13b710c5472
kapaman <amankapoor978>
2020-07-20 20:27:02 +0530

    fixing

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login');
 const resetRouter = require('./controllers/reset');
-const blogXROUTER = require('./controllers/blogXROUTER');
+
 
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.getToken)
 //blogrouter just provides a generic/default url like /api/blogs so you dont write it again n again
